94f7b78b9943e5bf5a02b9c10e888534b64ba3aa
Javier Labrador <jlabradorchacon@gmail.com>
2021-07-13 23:51:42 -0400

    Part 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react'
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickgood = () => {
+    setGood(prevGood => prevGood + 1)
+  }
+  const handleClickneutral = () => {
+    setNeutral(prevGood => prevGood + 1)
+  }
+  const handleClickbad = () => {
+    setBad(prevGood => prevGood + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleClickgood}>good</button>
+      <button onClick={handleClickneutral}>neutral</button>
+      <button onClick={handleClickbad}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +43,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
