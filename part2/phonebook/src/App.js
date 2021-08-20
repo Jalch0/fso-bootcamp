35d09afa6abc5d94507ac1247b21376494c2604c
Javier Labrador <jlabradorchacon@gmail.com>
2021-08-19 22:54:55 -0400

    Finish exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react'
 
 const App = () => {
+
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number:'040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number:'040-1234567' },
+    { name: "Ada Lovelace", number: "39-44-5323523"},
+    { name: "Dan Abramov", number: "12-43-234345"},
+    { name: "Mary Poppendieck", number: "39-23-6423122"}])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const [ newFilter, setNewFilter] = useState('')
+
+
+const searchfilter = (event) => {
+  setNewFilter(event.target.value)
+}
 
 const handleAddName = (event) => {
   setNewName(event.target.value)
@@ -19,8 +29,6 @@ const handleSubmit = (event) => {
     event.preventDefault()
 
     const findNameEqual = persons.some(person => person.name === newName);
-
-    console.log(persons.some(persons => persons.name === newName))
 
     if(findNameEqual) {
       alert(`${newName} is already added to phonebook `)
@@ -38,6 +46,8 @@ const handleSubmit = (event) => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with: <input type="text" value={newFilter} onChange={searchfilter}/></div>
+      <h2>add a new</h2>
       <form>
         <div>name: <input type="text" value={newName} onChange={handleAddName}/></div>
         <div>number: <input type="text" value={newNumber} onChange={handleAddNumber} /></div>
@@ -46,7 +56,11 @@ const handleSubmit = (event) => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {persons
+      .filter((person) => {
+        return person.name.toLowerCase().indexOf(newFilter.toLowerCase()) !== -1
+      })
+      .map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
